f895bf8e83b67428248bd4ea26cdc2214bea65a8
Chris Moeller <kode54@gmail.com>
2016-01-20 01:01:30 -0500

    Add MPAAP language file

---
+++ languages/english/MPAAP.english.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @author  ahrasis http://elk.ahrasis.com
+ * @license BSD http://opensource.org/licenses/BSD-3-Clause
+ * @mod     MPAAP - Modify Posts At Anytime Permission
+ *
+ */ 
+
+$txt['permissionname_modify_anytime'] = 'Modify posts at anytime';
+$txt['permissionhelp_modify_anytime'] = 'Modify posts at anytime means user in a group with this permission is not limited by time set by admin like all other group member.';
+
+?>
